e14e82af2a3f70ca387609dda62f2074362f05e0
jrojano28 <jrojano2809@gmail.com>
2024-11-24 13:39:52 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+def sumar(a, b):
+    # Implementar más adelante
+    pass
+
+def restar(a, b):
+    # Implementar más adelante
+    pass
+
+def multiplicar(a, b):
+    # Implementar más adelante
+    pass
+
+def dividir(a, b):
+    # Implementar más adelante
+    pass
+
+if __name__ == "__main__":
+    print("Calculadora sencilla")
+    # Aquí se agregarán las llamadas a las funciones
